57bed9c54538e18de31845d0a111f703373c26be
juan67965 <jcuenca129431@gmail.com>
2023-07-10 19:37:01 -0400

    add

---
+++ src/Carousel.js
@@ -0,0 +1,66 @@
+import { useState } from "react";
+import "./Carousel.css";
+import Card from "./Card";
+
+
+/** Carousel: displays images and arrows to navigate through them
+ * 
+ * Props:
+ * - photos: array of {src, caption} objects
+ * - title: string describing the collection of images
+ * 
+ * State:
+ * - currCardIdx: integer for current card index
+ * 
+ * App --> Carousel --> Card
+ */
+ function Carousel({ photos, title }) {
+  const [currCardIdx, setCurrCardIdx] = useState(0);
+
+  const currCard = photos[currCardIdx];
+  const total = photos.length;
+
+  //Increments currCardIdx state by 1
+  function goForward() {
+    if (currCardIdx !== 2){
+        setCurrCardIdx(currCardIdx + 1);
+    } else {
+      setCurrCardIdx(0);
+    }
+    
+  } 
+
+  function goBackward() {
+    if (currCardIdx !== 0) {
+      setCurrCardIdx(currCardIdx - 1);
+    } else {
+      setCurrCardIdx(photos.length - 1);
+    }
+  }
+
+  
+
+  return (
+    <div className="Carousel">
+      <h1>{title}</h1>
+      <div className="Carousel-main">
+        <i
+          className="bi bi-arrow-left-circle"
+          onClick={goBackward}
+        />
+        <Card
+          caption={currCard.caption}
+          src={currCard.src}
+          currNum={currCardIdx + 1}
+          totalNum={total}
+        />
+        <i
+          className="bi bi-arrow-right-circle"
+          onClick={goForward}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Carousel;
---
+++ src/Carousel.test.js
@@ -0,0 +1,78 @@
+import { render, fireEvent } from "@testing-library/react";
+import Carousel from "./Carousel";
+import TEST_IMAGES from "./_testCommon.js";
+
+it("renders without crashing", () => {
+  render(<Carousel photos = {TEST_IMAGES} />);
+});
+
+it("works when you click on the right arrow", function() {
+  const { container } = render(
+    <Carousel
+      photos={TEST_IMAGES}
+      title="images for testing"
+    />
+  );
+  // expect the first image to show, but not the second
+  expect(
+    container.querySelector('img[alt="testing image 1"]')
+  ).toBeInTheDocument();
+  expect(
+    container.querySelector('img[alt="testing image 2"]')
+  ).not.toBeInTheDocument();
+
+  // move forward in the carousel
+  const rightArrow = container.querySelector(".bi-arrow-right-circle");
+  fireEvent.click(rightArrow);
+
+  // expect the second image to show, but not the first
+  expect(
+    container.querySelector('img[alt="testing image 1"]')
+  ).not.toBeInTheDocument();
+  expect(
+    container.querySelector('img[alt="testing image 2"]')
+  ).toBeInTheDocument();
+
+  fireEvent.click(rightArrow);
+
+  expect(
+    container.querySelector('img[alt="testing image 2"]')
+  ).not.toBeInTheDocument();
+  expect(
+    container.querySelector('img[alt="testing image 3"]')
+  ).toBeInTheDocument();
+});
+
+it("works when you click on the left arrow", function () {
+  const { container } = render(
+    <Carousel photos={TEST_IMAGES} title="images for testing" />
+  );
+  const rightArrow = container.querySelector(".bi-arrow-right-circle");
+  fireEvent.click(rightArrow);
+
+  expect(
+    container.querySelector('img[alt="testing image 2"]')
+  ).toBeInTheDocument();
+  expect(
+    container.querySelector('img[alt="testing image 1"]')
+  ).not.toBeInTheDocument();
+
+  const leftArrow = container.querySelector(".bi-arrow-left-circle");
+  fireEvent.click(leftArrow);
+
+  expect(
+    container.querySelector('img[alt="testing image 2"]')
+  ).not.toBeInTheDocument();
+  expect(
+    container.querySelector('img[alt="testing image 1"]')
+  ).toBeInTheDocument();
+
+  fireEvent.click(leftArrow);
+
+  expect(
+    container.querySelector('img[alt="testing image 1"]')
+  ).not.toBeInTheDocument();
+  expect(
+    container.querySelector('img[alt="testing image 3"]')
+  ).toBeInTheDocument();
+});
